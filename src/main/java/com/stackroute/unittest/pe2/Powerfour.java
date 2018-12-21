a8d54667e5c37514ca7f05cc0c0a0ed726b62b86
Rohit <roniepatil@gmail.com>
2018-12-21 09:17:50 +0530

    first

---
+++ src/main/java/com/stackroute/unittest/pe2/Powerfour.java
@@ -0,0 +1,17 @@
+package com.stackroute.unittest.pe2;
+public class Powerfour {
+    public String Powercheck(int number){
+
+        if(number == 0)
+            return "no";
+        while(number != 1)
+        {
+            if(number % 4 != 0) {
+                return "no";
+            }
+            number=number / 4;
+        }
+
+        return "yes";
+    }
+}
